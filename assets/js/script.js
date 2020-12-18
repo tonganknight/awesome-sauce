7e970d2f6d454e1d8d7c034896a56d81b05429a6
Malachi <malachialusa@gmail.com>
2020-12-18 15:49:12 -0700

    I pulled all the data we need to build, and declared variables for all of it. I also added basic JS outlines for the functions we will need.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,127 @@
-/*ingredientSearch*/
-var search = "fried rice"
 
 
-var apiUrl ="https://api.edamam.com/search?q=" + search + "&app_id=" + "04d0cb88" + "&app_key=" + "26b371c06377eb2bd8223951d66a129e"
+//our search function 
+    /*ingredientSearch*/
+
+        var search = "tomato"
+
+        var apiUrl ="https://api.edamam.com/search?q=" + search + "&app_id=" + "04d0cb88" + "&app_key=" + "26b371c06377eb2bd8223951d66a129e"
 
 
-fetch(apiUrl).then (function(response){
-    if (response.ok) {
-           
-        response.json().then(function(data) {
-            console.log(data)
+        fetch(apiUrl).then (function(response){
+            if (response.ok) {
+                
+                response.json().then(function(data) {
+                    console.log(data)
+
+                //object data 
+
+                //First Recipe
+
+                    //name for first recipe
+                var recipe1 = data.hits[0].recipe.label;
+                
+
+                    //img for recipe
+                var recipe1img = data.hits[0].recipe.image;
+            
+
+                    //link for recipe 
+                var recipe1link = data.hits[0].recipe.url;
+                
+
+
+                    //list of ingredients
+                var recipe1ingr =data.hits[1].recipe.ingredientLines;
+                
+
+                //Second Recipe
+
+                    //name for second recipe
+                    var recipe2 = data.hits[1].recipe.label;
+                    
+            
+                        //img for recipe
+                    var recipe2img = data.hits[1].recipe.image;
+                    
+            
+                        //link for recipe 
+                    var recipe2link = data.hits[1].recipe.url;
+                
+            
+            
+                        //list of ingredients
+                    var recipe2ingr =data.hits[1].recipe.ingredientLines;
+                    
+
+                    //Third Recipe
+
+                    //name for Third recipe
+                    var recipe3 = data.hits[2].recipe.label;
+                    
+            
+                        //img for recipe
+                    var recipe3img = data.hits[2].recipe.image;
+                    
+            
+                        //link for recipe 
+                    var recipe3link = data.hits[2].recipe.url;
+                    
+            
+            
+                        //list of ingredients
+                    var recipe3ingr = data.hits[2].recipe.ingredientLines;
+                    
+
+                    //Fourth Recipe
+
+                    //name for Third recipe
+                    var recipe4 = data.hits[3].recipe.label;
+                
+            
+                        //img for recipe
+                    var recipe4img = data.hits[3].recipe.image;
+                    
+            
+                        //link for recipe 
+                    var recipe4link = data.hits[3].recipe.url;
+                    
+            
+            
+                        //list of ingredients
+                    var recipe4ingr = data.hits[3].recipe.ingredientLines;
+                    
+
+                    //Fifth Recipe
+
+                    //name for Fifth recipe
+                    var recipe5 = data.hits[4].recipe.label;
+                    
+            
+                        //img for recipe
+                    var recipe5img = data.hits[4].recipe.image;
+                    
+            
+                        //link for recipe 
+                    var recipe5link = data.hits[4].recipe.url;
+                    
+            
+            
+                        //list of ingredients
+                    var recipe5ingr = data.hits[4].recipe.ingredientLines;
+            
+
+
+            //write data 
+
+
+                //(we need to produce our HTML and CSS before we tackle this)
+
+
+
+            //data storage for Fav list 
+
+
 
         
         });
@@ -22,6 +134,23 @@ fetch(apiUrl).then (function(response){
         alert("Unable to connect");
     }); 
 
+//}); 
 
+//Event Listeners 
 
+    //search listener 
+        /*document.getElementById("Ourseachbutton").addEventListener("click", function(){
+
+        //our searchfunction()
+
+        ]); */
     
+
+//drop logic 
+
+
+
+
+
+
+
